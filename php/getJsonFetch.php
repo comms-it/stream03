05d0658e77d4e77f73af5977f9c951414600e7de
diego <diego@diego-neon>
2021-04-30 15:19:11 +0200

    made all working

---
+++ php/getJsonFetch.php
@@ -0,0 +1,7 @@
+<?php
+ 
+$dataJson = file_get_contents('php://input');
+// echo "<h3>Il server ha ricevuto questo: </h3>";    
+// echo '<pre>'.$dataJson.'</pre>';
+echo $dataJson;
+
---
+++ php/getUrlEncodedData.php
@@ -0,0 +1,7 @@
+<?php 
+echo "<h3>Il server ha ricevuto questo: </h3>";
+
+echo "<pre>";
+//var_dump($_POST);
+print_r($_POST);
+echo "</pre>";
